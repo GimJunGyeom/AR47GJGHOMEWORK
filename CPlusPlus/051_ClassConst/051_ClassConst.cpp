265fdcbeac50c96734abf2c4c68e3fba81694f32
GimJunGyeom <highjun90@gmail.com>
2023-03-28 07:36:02 +0900

    김준겸 : HomeWork0327

---
+++ CPlusPlus/051_ClassConst/051_ClassConst.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+//맴버함수 뒤에는 const를 붙일 수 있다.
+
+class Player
+{
+public:
+    void function() const 
+        //이 클랫를 통해서 만들어진 객체는 변하지 않는 다는 뜻
+        //맴버함수에 첫전째 인자로 암시적으로 전달하는 const Player* this 를 const Player* const this로 한다는 문법이다.
+        //이 맴버함수는 객체의 어떤것도 바꾸지 않을 거라는 의도를 표현하는 것
+    {      
+    }
+};
+
+
+int main()
+{
+    
+}
---
+++ CPlusPlus/052_GlobalValue/052_GlobalValue.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+//c++은 전역의 존재 때문에 엄밀한 객체지향 언어가 아님. 오직 객체, 클래스로만 동작하는게 객체지향.
+class Monster
+{
+public:
+	//모든 객체가 공유하는 데이터가 필요할때 데이터 영역에 있는 static 변수 씀 그래서 막상 객체 안(스택영역)에는 없다. 그래서 static 변수는 클래스 이름으로도 접근이 가능함.
+	static int MonsterCount;
+	//즉 static은 같은 클래스의 객체들끼리만 공유하는 전역변수.
+
+
+
+private:
+	static int MonsterCount_pri; //몬스터 객체만 접근가능
+};
+
+int main()
+{
+	Monster::MonsterCount = 20;
+}
