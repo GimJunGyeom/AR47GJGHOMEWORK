61c451519b3800349b062fb993b5989d0fbc2b85
GimJunGyeom <highjun90@gmail.com>
2023-04-11 09:45:10 +0900

    김준겸 : HomeWork0410

--- CPlusPlus/HomeWork0410/Bomb.cpp
+++ CPlusPlus/HomeWork0410/Bomb.cpp
@@ -48,32 +48,32 @@ void Bomb::Render()
 		}
 		for (int j = GetPos().Y - 1; j <= GetPos().Y + 1; j++)  //세로 영역
 		{
-			int2 Explosure(GetPos().X, j);
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosure, RenderChar);
+			int2 Explosion(GetPos().X, j);
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosion, RenderChar);
 		}
 	}else if (Ex_Step_1 >= BoomCount && Ex_Step_2 < BoomCount)       //폭발 1단계
 	{
 		for (int i = GetPos().X - 2; i <= GetPos().X + 2; i++)
 		{
-			int2 Explosure(i, GetPos().Y);
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosure, RenderChar);
+			int2 Explosion(i, GetPos().Y);
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosion, RenderChar);
 		}
 		for (int j = GetPos().Y - 2; j <= GetPos().Y + 2; j++)
 		{
-			int2 Explosure(GetPos().X, j);
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosure, RenderChar);
+			int2 Explosion(GetPos().X, j);
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosion, RenderChar);
 		}
 	}else if (Ex_Step_2 >= BoomCount && Ex_Step_End < BoomCount)      //폭발 2단계
 	{
 		for (int i = GetPos().X - 3; i <= GetPos().X + 3; i++)
 		{
-			int2 Explosure(i, GetPos().Y);
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosure, RenderChar);
+			int2 Explosion(i, GetPos().Y);
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosion, RenderChar);
 		}
 		for (int j = GetPos().Y - 3; j <= GetPos().Y + 3; j++)
 		{
-			int2 Explosure(GetPos().X, j);
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosure, RenderChar);
+			int2 Explosion(GetPos().X, j);
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(Explosion, RenderChar);
 		}
 	}
 }
